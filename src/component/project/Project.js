86467fe4369f8c6becd0cd0fc0bab6e802ed7536
Pocas1982 <andres.lemme@gmail.com>
2020-11-22 20:32:39 -0300

    I continue with the styles

--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Project.scss";
 
 function Project(props) {
-  const { name } = props.data;
+  const { name, clone_url } = props.data;
   return (
     <div className="project_box">
       <div className="title_box">
         <li>{name}</li>
       </div>
-      <div>
-        <a href={props.data.clone_url}>Ver Repo</a>
+      <div className="btn_box">
+        <a className="btn" href={clone_url}>Ver Repo</a>
       </div>
     </div>
   );
